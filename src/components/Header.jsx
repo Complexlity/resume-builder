ffc7b23b4233c321896448d969bdaee760baad94
Complexlity <105590967+Complexlity@users.noreply.github.com>
2022-12-03 19:06:50 +0100

    Add custom font to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
     <div className="flex h-16 items-center justify-center bg-blue-700 px-6 text-center text-3xl font-bold text-gray-200">
-      <h1 className="flex-1">CV CREATOR</h1>
+      <h1 className="header-text flex-1">CV CREATOR</h1>
       <a
         href="https://github.com/Complexlity"
         target="_blank"
